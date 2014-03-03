ef60348bbc913f0f9ad226475e7897187308e333
nikolai <nikolai@NIKOLAI-MINT>
2014-03-03 14:14:47 +0100

    commit aant l'enlever les cast

--- src/MoteurGraphique/Main.java
+++ src/MoteurGraphique/Main.java
@@ -61,8 +61,13 @@ public class Main {
             gui = new GUI();
             gui.paint(bi);
             
-                        
-            File outputfile ;
+            parametre.shadow = Parameter.PHONG_SHADE;
+            bi = mg.draw();
+            gui = new GUI();
+            gui.paint(bi);      
+            
+            
+//            File outputfile ;
 
 //            for (int i = -1; i <= 1; i++) {
 //                for (int j = -1; j <= 1; j++) {
